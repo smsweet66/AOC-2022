944f06bbd11de33690ce06b16e490a2c8912c1a6
Sean <sweet.sean42@gmail.com>
2023-04-03 17:31:53 -0500

    finished p20

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod tasks;
 fn main()
 {
     let start = now();
-    println!("{}", tasks::p20::get_sum_indices("input/p20.txt"));
+    println!("{}", tasks::p20::get_sum_indices_keyed("input/p20.txt"));
     println!("Time: {}s", (now() - start)/1000.0);
 }
--- src/tasks/p20.rs
+++ src/tasks/p20.rs
@@ -5,7 +5,7 @@ use crate::tasks::helper::get_lines;
 //node for a circular doubly linked list
 struct Node
 {
-	node_value: i32,
+	node_value: i128,
 	prev: Option<Rc<RefCell<Node>>>,
 	next: Option<Rc<RefCell<Node>>>,
 }
@@ -13,28 +13,30 @@ struct Node
 impl Node
 {
 	//creates a new node
-	pub fn new(node_value: i32) -> Node
+	fn new(node_value: i128) -> Node
 	{ Node { node_value, prev: None, next: None } }
 }
 
 //circular doubly linked list that keeps references to each node in the order added
 struct CDLList
 {
+	dec_key: i128,
 	nodes: Vec<Rc<RefCell<Node>>>,
 }
 
 impl CDLList
 {
 	//creates a new circular doubly linked list
-	fn new() -> CDLList
-	{
-		CDLList { nodes: Vec::new() }
-	}
+	fn new(dec_key: i128) -> CDLList
+	{ CDLList { dec_key, nodes: Vec::new() } }
+
+	fn with_capacity(capacity: usize, dec_key: i128) -> CDLList
+	{ CDLList { dec_key, nodes: Vec::with_capacity(capacity) } }
 
 	//adds a new node to the list
-	fn insert(&mut self, value: i32)
+	fn insert(&mut self, value: i128)
 	{
-		let node = Rc::new(RefCell::new(Node::new(value)));
+		let node = Rc::new(RefCell::new(Node::new(value * self.dec_key)));
 		if self.nodes.len() == 0
 		{
 			node.borrow_mut().prev = Some(node.clone());
@@ -58,40 +60,52 @@ impl CDLList
 	{
 		for node in &self.nodes
 		{
-			//println!("moving node {}", node.borrow().node_value);
 			let mut node = node.clone();
 			let mut value = node.borrow().node_value;
 			//as the list is circular, we can reduce the number of movements by
 			//taking the remainder of the value and the length of the list
 			if value >= 0
-			{ value %= self.nodes.len() as i32; }
-			else
-			{ value = self.nodes.len() as i32 -((-value) % self.nodes.len() as i32) - 1; }
-
-			//swap node with the node to the right of it
-			for _ in 0..value
 			{
-				let mut next = node.borrow_mut().next.clone().unwrap();
-				let mut prev = node.borrow_mut().prev.clone().unwrap();
-				let mut next_next = next.borrow_mut().next.clone().unwrap();
+				value %= (self.nodes.len() - 1) as i128;
 
-				//println!("original: {}, {}, {}, {}", prev.borrow().node_value, node.borrow().node_value, next.borrow().node_value, next_next.borrow().node_value);
+				//swap node with the node to the right of it
+				for _ in 0..value
+				{
+					let mut next = node.borrow_mut().next.clone().unwrap();
+					let mut prev = node.borrow_mut().prev.clone().unwrap();
+					let mut next_next = next.borrow_mut().next.clone().unwrap();
 
-				node.borrow_mut().next = Some(next_next.clone());
-				node.borrow_mut().prev = Some(next.clone());
-				next.borrow_mut().next = Some(node.clone());
-				next.borrow_mut().prev = Some(prev.clone());
-				next_next.borrow_mut().prev = Some(node.clone());
-				prev.borrow_mut().next = Some(next.clone());
-
-				//println!("after move: {}, {}, {}, {}", prev.borrow().node_value, next.borrow().node_value, node.borrow().node_value, next_next.borrow().node_value);
+					node.borrow_mut().next = Some(next_next.clone());
+					node.borrow_mut().prev = Some(next.clone());
+					next.borrow_mut().next = Some(node.clone());
+					next.borrow_mut().prev = Some(prev.clone());
+					next_next.borrow_mut().prev = Some(node.clone());
+					prev.borrow_mut().next = Some(next.clone());
+				}
 			}
+			else
+			{
+				value = -(-value%(self.nodes.len() - 1) as i128);
 
-			//println!("{}", self.to_string());
+				//swap node with the node to the left of it
+				for _ in value..0
+				{
+					let mut next = node.borrow_mut().next.clone().unwrap();
+					let mut prev = node.borrow_mut().prev.clone().unwrap();
+					let mut prev_prev = prev.borrow_mut().prev.clone().unwrap();
+
+					node.borrow_mut().next = Some(prev.clone());
+					node.borrow_mut().prev = Some(prev_prev.clone());
+					prev.borrow_mut().next = Some(next.clone());
+					prev.borrow_mut().prev = Some(node.clone());
+					prev_prev.borrow_mut().next = Some(node.clone());
+					next.borrow_mut().prev = Some(prev.clone());
+				}
+			}
 		}
 	}
 
-	fn get_value_from_zero(&self, index: usize) -> i32
+	fn get_value_from_zero(&self, index: usize) -> i128
 	{
 		let mut node = self.nodes[0].clone();
 		while node.borrow().node_value != 0
@@ -100,7 +114,7 @@ impl CDLList
 			node = next;
 		}
 
-		for _ in 0..index%self.nodes.len()
+		for _ in 0..index
 		{
 			let next = node.borrow_mut().next.clone().unwrap();
 			node = next;
@@ -132,22 +146,47 @@ impl CDLList
 ///done in the order that the numbers initially appear in the file.
 ///This function then returns the sum of the numbers 1000, 2000, and 3000 after
 ///the 0 in the list.
-pub fn get_sum_indices(filename: &str) -> i32
+pub fn get_sum_indices(filename: &str) -> i128
 {
 	let lines = get_lines(filename);
-	let mut list = CDLList::new();
+	let mut list = CDLList::with_capacity(lines.len(), 1);
 	for line in lines
 	{ list.insert(line.parse().unwrap()); }
 
-	println!("{}", list.to_string());
-
 	list.mix();
-
-	println!("{}", list.to_string());
 
 	let mut sum = 0;
 	for i in 1..=3
-	{ sum += list.get_value_from_zero(1000*i); }
+	{
+		let result = list.get_value_from_zero(1000*i);
+
+		println!("{}: {}", 1000*i, result);
+
+		sum += result;
+	}
+
+	sum
+}
+
+pub fn get_sum_indices_keyed(filename: &str) -> i128
+{
+	let lines = get_lines(filename);
+	let mut list = CDLList::with_capacity(lines.len(), 811589153);
+	for line in lines
+	{ list.insert(line.parse().unwrap()); }
+
+	for _ in 0..10
+	{ list.mix(); }
+
+	let mut sum = 0;
+	for i in 1..=3
+	{
+		let result = list.get_value_from_zero(1000*i);
+
+		println!("{}: {}", 1000*i, result);
+
+		sum += result;
+	}
 
 	sum
 }
